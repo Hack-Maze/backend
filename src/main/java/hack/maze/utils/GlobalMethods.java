278ed9a26994937d7fa5c4c2ed5c412a73923963
ziadamer <ziadamer112233@gmail.com>
2024-05-31 19:19:26 +0300

    clean some code

--- src/main/java/hack/maze/utils/GlobalMethods.java
+++ src/main/java/hack/maze/utils/GlobalMethods.java
@@ -1,6 +1,5 @@
 package hack.maze.utils;
 
-import hack.maze.entity.AppUser;
 import hack.maze.entity.Maze;
 import hack.maze.entity.Page;
 import hack.maze.entity.Question;
@@ -10,22 +9,22 @@ import java.util.Objects;
 
 public class GlobalMethods {
 
-    public static void checkUserAuthority(String username, Maze targetMaze) throws AccessDeniedException {
-        if (!Objects.equals(username, targetMaze.getAuthor().getAppUser().getUsername())) {
+    private static void checkUserAuthority(String currentUsername, String userThatWantToGainAccess) throws AccessDeniedException {
+        if (!Objects.equals(currentUsername, userThatWantToGainAccess)) {
             throw new AccessDeniedException("Access denied!");
         }
+    }
+
+    public static void checkUserAuthority(String username, Maze targetMaze) throws AccessDeniedException {
+        checkUserAuthority(username, targetMaze.getAuthor().getAppUser().getUsername());
     }
 
     public static void checkUserAuthority(String username, Page tagetPage) throws AccessDeniedException {
-        if (!Objects.equals(username, tagetPage.getMaze().getAuthor().getAppUser().getUsername())) {
-            throw new AccessDeniedException("Access denied!");
-        }
+        checkUserAuthority(username, tagetPage.getMaze().getAuthor().getAppUser().getUsername());
     }
 
     public static void checkUserAuthority(String username, Question targetQuestion) throws AccessDeniedException {
-        if (!Objects.equals(username, targetQuestion.getPage().getMaze().getAuthor().getAppUser().getUsername())) {
-            throw new AccessDeniedException("Access denied!");
-        }
+        checkUserAuthority(username, targetQuestion.getPage().getMaze().getAuthor().getAppUser().getUsername());
     }
 
     public static String nullMsg(String s) {
