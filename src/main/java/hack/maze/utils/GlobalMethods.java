3829c95a9b6e4101d4086db8cbf3e63b674d1f38
Mostafa-ewida <71707534+Mostafa-ewida@users.noreply.github.com>
2024-06-03 14:24:24 +0300

    adding postman collection to source code(#10)
    * Add Postman Collection
    
    * Add Postman Collection
    
    * Update README.md
    
    * Rename hackMaze.postman_collection4.json to hackMaze.postman_collection.json
    
    * Update .dockerignore
    
    ---------
    
    Co-authored-by: Marym <Marymfoda138@gmail.com>

--- src/main/java/hack/maze/utils/GlobalMethods.java
+++ src/main/java/hack/maze/utils/GlobalMethods.java
@@ -3,10 +3,11 @@ package hack.maze.utils;
 import hack.maze.entity.Maze;
 import hack.maze.entity.Page;
 import hack.maze.entity.Question;
+import lombok.extern.slf4j.Slf4j;
 
 import java.nio.file.AccessDeniedException;
 import java.util.Objects;
-
+@Slf4j
 public class GlobalMethods {
 
     private static void checkUserAuthority(Long userThatWantToGainAccess, Long authorId) throws AccessDeniedException {
@@ -16,14 +17,17 @@ public class GlobalMethods {
     }
 
     public static void checkUserAuthority(Long id, Maze targetMaze) throws AccessDeniedException {
+        log.info("[{}] -- [{}]", id, targetMaze.getAuthor().getAppUser().getId());
         checkUserAuthority(id, targetMaze.getAuthor().getAppUser().getId());
     }
 
     public static void checkUserAuthority(Long id, Page tagetPage) throws AccessDeniedException {
+        log.info("[{}] -- [{}]",id, tagetPage.getMaze().getAuthor().getAppUser().getId() );
         checkUserAuthority(id, tagetPage.getMaze().getAuthor().getAppUser().getId());
     }
 
     public static void checkUserAuthority(Long id, Question targetQuestion) throws AccessDeniedException {
+        log.info("[{}] -- [{}]", id, targetQuestion.getPage().getMaze().getAuthor().getAppUser().getId());
         checkUserAuthority(id, targetQuestion.getPage().getMaze().getAuthor().getAppUser().getId());
     }
 
