9eef15d8aa11cfac5fc93141fdb9c2065be4d394
Marym <Marymfoda138@gmail.com>
2024-06-25 12:07:45 +0300

    Add isCompleted to getpages endpoint

--- src/main/java/hack/maze/dto/PageResponseDTO.java
+++ src/main/java/hack/maze/dto/PageResponseDTO.java
@@ -5,5 +5,5 @@ import lombok.Builder;
 import java.util.List;
 
 @Builder
-public record PageResponseDTO(Long id, String title, String description, String content, List<QuestionResponseDTO> questions) {
+public record PageResponseDTO(Long id, String title, String description, String content, List<QuestionResponseDTO> questions , boolean isCompleted) {
 }
